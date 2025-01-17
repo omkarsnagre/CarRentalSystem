03f99d4c1e14a0120f303d7753a13a7181d8e0df
Omkar Nagre <140543818+omkarsnagre@users.noreply.github.com>
2025-01-17 11:28:24 +0530

    Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 package com.carrental;
 
 import java.util.Scanner;
-
+// Main class
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
